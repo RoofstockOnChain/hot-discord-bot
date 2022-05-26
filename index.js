c908a06c6e2fe243c7c90a57595a7c0a9a5deb7f
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2022-05-25 23:39:43 -0500

    Add interaction create event

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const { Client, Intents } = require("discord.js");
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES],
+});
 
 client.once("ready", () => {
   console.log("Ready!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  console.log(interaction);
 });
 
 client.login(process.env.DISCORD_TOKEN);
